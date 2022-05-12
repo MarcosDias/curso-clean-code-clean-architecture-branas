0c450d00eb5e70fe28a7453770ffc2359a37209f
Marcos Dias <marcos.a.melo.dias@gmail.com>
2022-05-12 08:49:05 -0300

    Refatoração das condicionais de cláusulas guarda

--- 2_refactoring/test/v2/cpf.test.ts
+++ 2_refactoring/test/v2/cpf.test.ts
@@ -5,6 +5,26 @@ test("Deve validar um cpf válido", () => {
   expect(isValid).toBeTruthy();
 });
 
+const wrongSameDigitCpf = [
+  "111.111.111-11",
+  "222.222.222-22",
+  "333.333.333-33",
+  "444.444.444-44",
+  "555.555.555-55",
+  "666.666.666-66",
+  "777.777.777-77",
+  "888.888.888-88",
+  "999.999.999-99",
+];
+
+test.each(wrongSameDigitCpf)(
+  "Deve validar um cpf inválido com todos os números iguais",
+  (cpf) => {
+    const isNotValid = validate(cpf);
+    expect(isNotValid).toBeFalsy();
+  }
+);
+
 test("Deve validar um cpf inválido com todos os números iguais", () => {
   const isNotValid = validate("111.111.111-11");
   expect(isNotValid).toBeFalsy();
@@ -13,4 +33,9 @@ test("Deve validar um cpf inválido com todos os números iguais", () => {
 test("Deve validar um cpf inválido que seja nulo", () => {
   const isNotValid = validate(null);
   expect(isNotValid).toBeFalsy();
+});
+
+test("Deve validar um cpf válido com alguns pontos", () => {
+  const isValid = validate("935.411.34780");
+  expect(isValid).toBeTruthy();
 });
